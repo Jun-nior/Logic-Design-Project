024e40a2e20ea49412b5c1a2e55b547ba1b7bc88
Jun-nior <petertg1702@gmail.com>
2024-01-07 18:14:57 +0700

    update gui

--- box.py
+++ box.py
@@ -10,6 +10,9 @@ import time
 import requests
 import threading
 from Adafruit_IO import MQTTClient
+import customtkinter as custom
+
+
 
 AIO_KEY = "aio_AqXF50XhVsRavK3GlO6rBmd34V1V"
 AIO_USERNAME = "Unray"
@@ -106,8 +109,8 @@ def print_AI_result():
         timer1=4
     AI_stream.delete(1.0, tk.END)
     cs_stream.delete(1.0, tk.END)
-    AI_stream.insert(tk.END, "AI Result: " + command)
-    cs_stream.insert(tk.END, f"Confident score:  + {confident_score}")
+    AI_stream.insert(tk.END, command)
+    cs_stream.insert(tk.END, confident_score)
     
 
 
@@ -241,7 +244,7 @@ def prediction():
 
 # Create the main window
 root = tk.Tk()
-
+root.configure(bg="#ADD8E6")
 # Set the size of the window to 1000x500
 root.geometry("1000x500")
 
@@ -259,11 +262,13 @@ canvas.pack()
 # Create an Entry widget for the IP address
 frame_ip=tk.Frame(root)
 frame_ip.place(relx=0.25, rely=0.7, anchor="center")
-
+frame_ip.config(bg="#EFEFEF", bd=2, relief=tk.GROOVE)
 entry_var = tk.StringVar()
 entry_ip = tk.Entry(frame_ip, width=20, textvariable=entry_var)
-entry_ip.grid(row=0, column=0, padx=5)
+entry_ip.grid(row=0, column=1, padx=5)
 
+label_ip = tk.Label(frame_ip, text="Enter IP Address:", font=("Arial", 12, "bold"), bg="#FFFF00")
+label_ip.grid(row=0, column=0, padx=5)
 
 # Create a frame for the button
 frame_button = tk.Frame(root)
@@ -271,42 +276,42 @@ frame_button.pack(pady=10)
 frame_button.place(relx=0.25, rely=0.8, anchor="center")
 
 # Create a button to start/stop the video stream
-btn_toggle_stream = tk.Button(frame_button, text="Start/Stop Stream", command=check_ip)
+btn_toggle_stream = tk.Button(frame_button, text="Start/Stop Stream", command=check_ip, bg="#FFA500", fg="black", relief=tk.RAISED, borderwidth=5, font=("Calibri", 11))
 btn_toggle_stream.pack()
 
 # button up
 frame_up = tk.Frame(root)
 frame_up.pack(pady=10)
 frame_up.place(relx=0.75, rely=0.3, anchor="center")
-up_stream = tk.Button(frame_up, text="Up", command=up, width=10)
+up_stream = tk.Button(frame_up, text="\u2191", command=up, width=10, bg="#8FED8F", fg="black", relief=tk.RIDGE, borderwidth=5, font=("Calibri", 11, "bold"))
 up_stream.pack()
 
 #button down
 frame_down = tk.Frame(root)
 frame_down.pack(pady=10)
 frame_down.place(relx=0.75, rely=0.4, anchor="center")
-down_stream = tk.Button(frame_down, text="Down", command=down, width=10)
+down_stream = tk.Button(frame_down, text="\u2193", command=down, width=10, bg="#8FED8F", fg="black", relief=tk.RIDGE, borderwidth=5, font=("Calibri", 11, "bold"))
 down_stream.pack()
 
 #button left
 frame_left = tk.Frame(root)
 frame_left.pack(pady=10)
 frame_left.place(relx=0.65, rely=0.4, anchor="center")
-left_stream = tk.Button(frame_left, text="Left", command=left, width=10)
+left_stream = tk.Button(frame_left, text="\u2190", command=left, width=10, bg="#8FED8F", fg="black", relief=tk.RIDGE, borderwidth=5, font=("Calibri", 11, "bold"))
 left_stream.pack()
 
 #button right
 frame_right = tk.Frame(root)
 frame_right.pack(pady=10)
 frame_right.place(relx=0.85, rely=0.4, anchor="center")
-right_stream = tk.Button(frame_right, text="Right", command=right, width=10)
+right_stream = tk.Button(frame_right, text="\u2192", command=right, width=10, bg="#8FED8F", fg="black", relief=tk.RIDGE, borderwidth=5, font=("Calibri", 11, "bold"))
 right_stream.pack()
 
 #button automatic
 frame_automatic = tk.Frame(root)
 frame_automatic.pack(pady=10)
 frame_automatic.place(relx=0.75, rely=0.5, anchor="center")
-automatic_stream = tk.Button(frame_automatic, text="Automatic Run", command=automatic, width=40)
+automatic_stream = tk.Button(frame_automatic, text="Automatic Run", command=automatic, width=40, bg="#FFFF00", fg="black", relief=tk.GROOVE, borderwidth=5, font=("Calibri", 11))
 automatic_stream.pack()
 
 #print AI result
@@ -314,14 +319,20 @@ frame_ai=tk.Frame(root)
 frame_ai.pack(pady=10)
 frame_ai.place(relx=0.75, rely=0.6, anchor="center")
 AI_stream = tk.Text(frame_ai, height=1, width=30)
-AI_stream.pack()
+AI_stream.grid(row=0,column=1,padx=5)
+label_ai=tk.Label(frame_ai, text="AI result: ", font=("Arial", 11), bg="#8FED8F")
+label_ai.grid(row=0,column=0,padx=5)
+# AI_stream.pack()
 
 #print confident score
 frame_cs=tk.Frame(root)
 frame_cs.pack(pady=10)
 frame_cs.place(relx=0.75, rely=0.7, anchor="center")
 cs_stream = tk.Text(frame_cs, height=1, width=30)
-cs_stream.pack()
+cs_stream.grid(row=0,column=1,padx=5)
+label_cs=tk.Label(frame_cs, text="Confident score: ", font=("Arial", 11), bg="#8FED8F")
+label_cs.grid(row=0,column=0,padx=5)
+# cs_stream.pack()
 
 # Call the update_video function to start displaying the video feed
 update_video_thread = threading.Thread(target=update_video)
